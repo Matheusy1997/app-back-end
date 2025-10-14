32b16ea4ae959dcdb3d25e1d289555a24e1956e6
Matheusy1997 <matheusmachado.dev@gmail.com>
2025-10-14 15:47:05 -0300

    criado o service de post para manipular o db

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,2 +1,30 @@
-import { prisma } from "../lib/prisma.js"
+import type { Post } from "@prisma/client";
+import { prisma } from "../lib/prisma.js";
 
+export const PostService = {
+  getAllPosts: async (): Promise<Post[] | null> => {
+    return prisma.post.findMany({ where: {published: true } });
+  },
+
+  getPostsByTitle: async (title: string): Promise<Post[] | null> => {
+    return prisma.post.findMany({ where: { title, published: true } });
+  },
+
+  getPostByAuthor: async(authorId: number): Promise<Post[] | null> => {
+    return prisma.post.findMany({ where: { authorId } })
+  },
+
+  create: async (
+    post: Omit<Post, "id" | "createdAt" | "updatedAt">
+  ): Promise<Post> => {
+    return prisma.post.create({ data: post });
+  },
+
+  deletePostById: async (id: number): Promise<Post> => {
+    return prisma.post.delete({ where: { id } });
+  },
+
+  updatePost: async(id:number, post: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">): Promise<Post> => {
+    return prisma.post.update({ where: {id}, data: {title: post.title, content: post.content, published: post.published}})
+  },
+};
